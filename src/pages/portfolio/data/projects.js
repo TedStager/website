5f1202dbbb34c2ddc95dd80a27c04bf8408591e5
twstager <twstager@uwaterloo.ca>
2023-08-02 12:00:38 -0400

    added basic portfolio tiles

---
+++ src/pages/portfolio/data/projects.js
@@ -0,0 +1,12 @@
+export const projects = [
+	{
+		title: "Arduino Pong Game",
+		subtitle: "Arduino, SPI, C++",
+		description: "Made a recreation of the classic arcade game Pong on an using an SPI display.",
+		image: "./apple.png",
+		image_desc: "An apple",
+		link: "/",
+	}
+
+
+];
--- src/pages/portfolio/portfolio.js
+++ src/pages/portfolio/portfolio.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import Navbar from '../../components/navbar/navbar.js'
+import Navbar from '../../components/navbar/navbar.js';
 import './portfolio.css';
+import { projects } from './data/projects.js';
 
 const Portfolio = () => {
 	return (
 		<>
 			<Navbar />
-			<h1 className="comingSoon"> Coming soon! </h1>
+			<div className="projPortfolio">
+				<h1 className="portfolioHeader">
+					Check out some of my projects!
+				</h1>
+
+				<div className="projectList">
+					{
+						projects.map((project) => (
+							<a
+							href={project.link}
+							key={project.image}
+							className="projectTile">
+								<div className="projectTileContents">
+									<img
+									alt={project.image_desc}
+									className="projectImage"
+									src={project.image}
+									/>
+
+									<div className="projectText">
+										<h2 className="projectSub">
+											{project.subtitle}
+										</h2>
+										<h1 className="projectTitle">
+											{project.title}
+										</h1>
+										<p className="projectDesc">
+											{project.description}
+										</p>
+									</div>
+								</div>
+							</a>
+						))
+					}
+				</div>
+			</div>
 		</>
 	);
 };
