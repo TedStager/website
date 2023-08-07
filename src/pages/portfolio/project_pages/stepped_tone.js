d67a8cdb82a845c02deffd487aa34a4d467b277c
Ted Stager <twstager@gmail.com>
2023-08-07 18:43:44 -0400

    added pages for projects

---
+++ src/pages/portfolio/project_pages/stepped_tone.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../../components/navbar/navbar.js';
+import './project_pages.css';
+
+const SteppedTone = () => {
+	return (
+		<>
+			<Navbar />
+			<h1 className="heading"> Coming Soon! </h1>
+		</>
+	);
+}
+
+export default SteppedTone;
