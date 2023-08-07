f9d0bd793950f17ed6efb03e0a474a095fa7e839
Ted Stager <twstager@gmail.com>
2023-08-07 18:46:41 -0400

    updated links for projects

---
+++ src/pages/portfolio/projects.js
@@ -0,0 +1,45 @@
+// JSON object to store project data in
+
+export const projects = [
+	{
+		title: "Arduino Pong Game",
+		subtitle: "Arduino, SPI, C++",
+		description: "Made a recreation of the classic arcade game Pong on an Arduino using an SPI display.",
+		image: "./data/apple.png",
+		image_desc: "An apple",
+		link: "https://github.com/TedStager/Arduino-Pong",
+	},
+	{
+		title: "C++ Connect-4 Engine",
+		subtitle: "C++, Algorithm Design",
+		description: "Made a Connect-4 player in C++ using a variant of a min-max algorithm.",
+		image: "./data/portfolio/connect_four.png",
+		image_desc: "Screenshot from game",
+		link: "https://github.com/TedStager/Connect-4-Solver",
+	},
+	{
+		title: "Virtual Enigma Machine",
+		subtitle: "Python, tkinter GUI",
+		description: "Designed and programmed a fully functional digital recreation of the Enigma Machine",
+		image: "./data/portfolio/enigma_machine.png",
+		image_desc: "Screenshot of application",
+		link: "https://github.com/TedStager/Virtual-Enigma-Machine",
+	},
+	{
+		title: "Personal Portfolio Website",
+		subtitle: "ReactJS, HTML/CSS",
+		description: "This website! Fully built from scratch with React and CSS for styling.",
+		image: "./data/portfolio/website_thumbnail.png",
+		image_desc: "Website home page",
+		link: "https://github.com/TedStager/website"
+	},
+	{
+		title: "Stepped Tone Generator",
+		subtitle: "ICs, Circuit Design",
+		description: "Built a basic analogue synthesizer from two 555 timers.",
+		image: "./data/portfolio/stepped_tone.png",
+		image_desc: "Picture of the synthesizer",
+		link: "/portfolio/stepped-tone-generator",
+	}
+
+];
