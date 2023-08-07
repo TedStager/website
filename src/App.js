54deb2333ee891462f536fd5f410cdb0cc19dbad
Ted Stager <twstager@gmail.com>
2023-08-07 18:44:26 -0400

    added a page to router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home.js';
 import About from './pages/about/about.js';
 import Portfolio from './pages/portfolio/portfolio.js'
+import SteppedTone from './pages/portfolio/project_pages/stepped_tone.js'
 import './App.css';
 
 function App() {
@@ -10,8 +11,10 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/about" exact element={<About />} />     
-        <Route exact path="/" exact element={<Home /> } />
+        <Route exact path="/" exact element={<Home />} />
         <Route exact path="/portfolio" exact element={<Portfolio />} />
+
+        <Route exact path="/portfolio/stepped-tone-generator" exact element={<SteppedTone />} />
       </Routes>
     </Router>
   );
